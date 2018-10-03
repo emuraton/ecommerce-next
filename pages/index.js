2ccc3754fda0f084c38987908a9e2c7a689f8ceb
Emilien Muraton <emilien.muraton@gmail.com>
2018-10-03 09:58:54 +0100

    add Header component

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <div>Welcome to next.js!</div>;
+import React, { Fragment } from 'react';
+import Header from '../components/Header/Header';
+
+const Page = () => {
+  return (
+    <Fragment>
+      <Header />
+      <p>Welcome to next.js!</p>
+    </Fragment>
+  );
+};
+export default Page;
