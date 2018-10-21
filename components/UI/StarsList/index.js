6dbc1af11746c9cb0c0afb0f5a341f9ffdbb779b
Emilien Muraton <emilien.muraton@gmail.com>
2018-10-21 17:56:23 +0100

    add: <StarsList/>

---
+++ components/UI/StarsList/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { prop } from 'styled-tools';
+
+import Star from '../Star';
+
+const Container = styled.div`
+  display: inline-block;
+`;
+
+const StyledStars = styled(Star)`
+  margin-right: 2px;
+  fill: ${prop('color')};
+`;
+StyledStars.displayName = 'Stars';
+
+const StarsList = ({ nbStars, color, className }) => {
+  if (!nbStars || nbStars === 0) return null;
+  return (
+    <Container className={className}>
+      {new Array(nbStars).fill(StyledStars).map((Star, index) => (
+        <Star key={`Star-${index}`} color={color} />
+      ))}
+    </Container>
+  );
+};
+
+StarsList.defaultProps = {
+  nbStars: 0,
+  color: 'black',
+};
+
+export default StarsList;
---
+++ components/UI/StarsList/tests/index.test.js
@@ -0,0 +1,28 @@
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
+
+import StarsList from '../';
+
+describe('<StarsList />', () => {
+  describe('@renders', () => {
+    it('default render', () => {
+      const wrapper = shallow(<StarsList />);
+      expect(toJson(wrapper)).toBe('');
+    });
+
+    it('5 stars', () => {
+      const props = { nbStars: 5 };
+      const wrapper = shallow(<StarsList {...props} />);
+      const stars = wrapper.find('Stars');
+      expect(stars).toHaveLength(5);
+    });
+
+    it('color=red', () => {
+      const props = { nbStars: 2, color: 'red' };
+      const wrapper = mount(<StarsList {...props} />);
+      const star = wrapper.find('Stars').first();
+      expect(star).toHaveStyleRule('fill', 'red');
+    });
+  });
+});
