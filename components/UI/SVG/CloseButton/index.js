4ca9b376fa38e82a443e95a21905e827ef28e66a
Emilien Muraton <emilien.muraton@gmail.com>
2018-10-27 21:34:28 +0100

    add: closeButton svg

---
+++ components/UI/SVG/CloseButton/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CloseButton = ({ className }) => (
+  <svg width={18} height={18} viewBox="0 0 18 18" className={className}>
+    <path d="M16 14.7l-5.6-5.6L16 3.5l-1.4-1.4L9 7.7 3.4 2.1 2 3.5l5.6 5.6L2 14.7l1.4 1.4L9 10.5l5.6 5.6z" />
+  </svg>
+);
+
+export default CloseButton;
