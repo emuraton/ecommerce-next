a0be54070b35eaafa9c0c1561025bdc941c2f57c
Emilien Muraton <emilien.muraton@gmail.com>
2018-11-03 00:08:12 +0000

    hack: hardcode map link

--- components/UI/SubHeaderProduct/index.js
+++ components/UI/SubHeaderProduct/index.js
@@ -16,6 +16,9 @@ const amenities = [
 const SubHeaderProduct = ({ product }) => {
   const { name, distance, description, type, address } = product;
   const isTicket = type === 'ticket';
+  const mapLink = isTicket
+    ? `https://www.google.com/maps/place/48%C2%B041'36.2%22N+9%C2%B011'15.4%22E/@48.693397,9.1854283,16z/data=!4m5!3m4!1s0x0:0x0!8m2!3d48.693397!4d9.187617`
+    : `https://www.google.com/maps/dir/?api=1&destination=48.693397,9.187617&origin=48.793102,9.191008`;
 
   return (
     <SubHeader.Wrapper>
@@ -25,7 +28,7 @@ const SubHeaderProduct = ({ product }) => {
         <SubHeader.LinkMap
           target="_blank"
           rel="noopener noreferrer nofollow"
-          href="https://www.google.com/maps/dir/?api=1&destination=48.693397,9.187617&origin=48.793102,9.191008"
+          href={mapLink}
         >
           <MapMarker />
           <SubHeader.FestivalName>
@@ -36,10 +39,7 @@ const SubHeaderProduct = ({ product }) => {
           )}
         </SubHeader.LinkMap>
       </SubHeader.MarginWrapper>
-      <UglyMap
-        src="/static/staticmap.jpg"
-        link="https://www.google.com/maps/dir/?api=1&destination=48.693397,9.187617&origin=48.793102,9.191008"
-      />
+      <UglyMap src="/static/staticmap.jpg" link={mapLink} />
       {!isTicket && <AmenitiesRow amenities={amenities} />}
       <SubHeader.Paragraph>{description}</SubHeader.Paragraph>
     </SubHeader.Wrapper>
