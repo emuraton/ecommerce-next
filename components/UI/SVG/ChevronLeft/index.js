f7b1d592411dffbdb5bbec3d7c9351788b0cfd40
Emilien Muraton <emilien.muraton@gmail.com>
2018-11-04 14:58:48 +0000

    fix: error for SVGs

--- components/UI/SVG/ChevronLeft/index.js
+++ components/UI/SVG/ChevronLeft/index.js
@@ -1,9 +1,17 @@
+// @flow
 import React from 'react';
 
-const ChevronLeft = ({ className }) => (
+type Props = {
+  className?: string,
+};
+
+const ChevronLeft = ({ className }: Props) => (
   <svg width={18} height={18} viewBox="0 0 18 18" className={className}>
     <path d="M12.2 14.7L6.6 9l5.6-5.7-1.4-1.4-7 7.1 7 7.1z" />
   </svg>
 );
+ChevronLeft.defaultProps = {
+  className: '',
+};
 
 export default ChevronLeft;
--- components/UI/SVG/Star/index.js
+++ components/UI/SVG/Star/index.js
@@ -1,15 +1,17 @@
+// @flow
 import React from 'react';
 
-const Star = ({ className }) => (
-  <svg
-    height={14}
-    width={14}
-    viewBox="0 0 14 14"
-    fill="black"
-    className={className}
-  >
+type Props = {
+  className?: string,
+};
+
+const Star = ({ className }: Props) => (
+  <svg height={14} width={14} viewBox="0 0 14 14" fill="black" className={className}>
     <path d="M7 11.4l-4.3 2.3.8-4.9L0 5.4l4.8-.7L7 .3l2.2 4.4 4.8.7-3.5 3.4.8 4.9z" />
   </svg>
 );
+Star.defaultProps = {
+  className: '',
+};
 
 export default Star;
