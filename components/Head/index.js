d6abbb0b04b6a89e25c016da026f049c58728c6f
Emilien Muraton <emilien.muraton@gmail.com>
2018-12-29 15:36:09 +0000

    add: locale font

--- components/Head/index.js
+++ components/Head/index.js
@@ -9,12 +9,7 @@ const Meta = ({ title }: Props) => (
   <Head>
     <title>{title}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link
-      rel="preload"
-      href="https://fonts.googleapis.com/css?family=Helvetica:300,400,700,800"
-      as="font"
-      type="font/woff2"
-    />
+    <link rel="preload" href="/static/fonts/helvetica.woff2" as="font" type="font/woff2" />
   </Head>
 );
 export default Meta;
